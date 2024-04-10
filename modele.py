eb4e8042995b4f627ad349e77198da5f1888d5c2
coall132 <kaeligroche132@gmail.com>
2024-04-10 10:43:57 +0200

    a

--- modele.py
+++ modele.py
@@ -1,4 +1,43 @@
-def afficher():
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float, Date, select, update, delete
+from sqlalchemy.orm import relationship, sessionmaker, Session, mapped_column, declarative_base
+from sqlalchemy import create_engine
+import os, dotenv, requests, datetime, json, math, subprocess, re, glob
 
-    print("start modele")
-    return
+dotenv.load_dotenv()
+BDD_URL=os.getenv('DATABASE_URL')
+# Connection à la BDD
+engine = create_engine(BDD_URL) # , echo=True
+# classe de base dont no objets ORM vont dériver
+Base = declarative_base()
+
+class Client(Base):
+    __tablename__ = 'clients'
+    id  = Column(Integer, primary_key=True)
+    name = Column(String)
+    adr = Column(String)
+    cat = Column(String)
+    # 'factures' permet d'accéder aux factures (1..N) du clients
+    factures = relationship("Facture", back_populates="client")
+
+    def __str__(this):
+        return f"CLIENT [{this.id}] {this.name} ({this.adr})"
+
+class Facture(Base):
+    __tablename__ = 'factures'
+    no = Column(String, primary_key=True)
+    dt = Column(DateTime)
+    total = Column(Float)
+    # client_id est la FK
+    client_id = mapped_column(ForeignKey("clients.id"))
+    # 'client' permet d'accéder au client lié à la facture
+    client = relationship("Client", back_populates="factures")
+
+    # Et les commandes ???
+
+    def __str__(this):
+        return f"FACTURE [{this.no}] {this.total}€"
+
+
+
+
+Base.metadata.create_all(bind=engine)
