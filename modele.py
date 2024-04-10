7645a0940cc3526010e2b45be0d3578d09e6ac36
coall132 <kaeligroche132@gmail.com>
2024-04-10 10:49:32 +0200

    aa

--- modele.py
+++ modele.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float, Date, select, update, delete
 from sqlalchemy.orm import relationship, sessionmaker, Session, mapped_column, declarative_base
 from sqlalchemy import create_engine
-import os, dotenv, requests, datetime, json, math, subprocess, re, glob
+import os, dotenv, requests, datetime, json, math, subprocess, re, glob, urllib, pyodbc
 
 dotenv.load_dotenv()
 BDD_URL=os.getenv('DATABASE_URL')
+quoted_params = urllib.parse.quote_plus(BDD_URL)
+
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(quoted_params)
 # Connection à la BDD
-engine = create_engine(BDD_URL) # , echo=True
+engine = create_engine(conn_str, echo=True) # , echo=True
 # classe de base dont no objets ORM vont dériver
 Base = declarative_base()
 
